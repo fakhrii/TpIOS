7ed340a0f66da42a5d072fafe1e41a63db65d106
fakhrii <fakhri_kraiem@hotmail.fr>
2021-01-07 13:25:28 +0100

    ios

---
+++ Restaurants/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  Restaurants
+//
+//  Created by EPI on 12/12/2020.
+//  Copyright © 2020 epi. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Restaurants/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Restaurants
+//
+//  Created by EPI on 12/12/2020.
+//  Copyright © 2020 epi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
